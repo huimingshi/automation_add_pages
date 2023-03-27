6bfff108276aea11a79cd900cfecdc1e75e221eb
huimingshi <843361731@qq.com>
2023-03-27 09:47:38 +0800

    开发新脚本

--- Citron/scripts/Calls/call_test_case/call_python_Lib/call_check_lib.py
+++ Citron/scripts/Calls/call_test_case/call_python_Lib/call_check_lib.py
@@ -209,7 +209,7 @@ def check_in_photo_pdf_whiteboard_mode(mode,*drivers):
     """
     for i in range(len(drivers)):
         if mode == 'pdf':
-            get_xpath_element(drivers,share_page_button,description=f"第{i + 1}个driver处于pdf模式中")
+            get_xpath_element(drivers[i],share_page_button,description=f"第{i+1}个driver处于pdf模式中")
         elif mode == 'photo' or mode == 'whiteboard':
             ele_list = get_xpath_elements(drivers[i],zoom_in_photo)
             public_assert(drivers[i],len(ele_list),1,action=f"第{i+1}个driver处于photo或whiteboard模式中")
